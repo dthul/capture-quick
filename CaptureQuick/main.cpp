ed96a8f6e82ad02871cb77099036b61d832838df
Daniel Thul <daniel.thul@gmail.com>
2015-05-13 10:44:31 +0300

    Set up environment variables for libgphoto2 properly and added a README.DEPLOY that outlines the deploy steps for Mac OS X.

--- CaptureQuick/main.cpp
+++ CaptureQuick/main.cpp
@@ -18,12 +18,19 @@
 
 int main(int argc, char *argv[])
 {
-    const QFileInfo executable_path(QString::fromStdString(util::executable_path()));
-    const std::string lib_dir = executable_path.absolutePath().toStdString();
-    util::setenv("IOLIBS", lib_dir);
-    util::setenv("CAMLIBS", lib_dir);
 #ifdef __APPLE__
+    const QFileInfo executable_path(QString::fromStdString(util::executable_path()));
+    QDir iolib_dir = executable_path.absoluteDir();
+    iolib_dir.cd("../Frameworks/libgphoto2_port/0.12.0/");
+    QDir camlib_dir = executable_path.absoluteDir();
+    camlib_dir.cd("../Frameworks/libgphoto2/2.5.7/");
+    std::cout << "IOLIBS" << iolib_dir.absolutePath().toStdString() << std::endl;
+    std::cout << "CAMLIBS" << camlib_dir.absolutePath().toStdString() << std::endl;
+    util::setenv("IOLIBS", iolib_dir.absolutePath().toStdString());
+    util::setenv("CAMLIBS", camlib_dir.absolutePath().toStdString());
     system("killall PTPCamera");
+#else
+#warning "No libgphoto2 environment variables specified for this operating system. If you want to deploy the application stand-alone you probably have to set them."
 #endif
     QApplication app(argc, argv);
     app.setOrganizationName("Aalto");
